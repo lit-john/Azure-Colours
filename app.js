feedf64ef9380c3025944ed4d098e7088e053d25
lit-john <john.hannafin@lit.ie>
2014-03-26 23:16:44 +0000

    Added the main body on initial code

--- app.js
+++ app.js
@@ -5,16 +5,44 @@
 
 var express = require('express');
 var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
 var app = express();
 
+// Let's require the mongodb package
+var mongo = require('mongodb');
+
+// Get the MongoClient Object
+var mongoClient = mongo.MongoClient;
+
+var MONGODB_URI = process.env.CUSTOMCONNSTR_MONGODB_URI || 'mongodb://localhost:27017/colours';
+
+// Connect to the db. The callback function will be passed two arguments: err - which
+// will contain error information, and db - which will contain a connection to the
+// mongodb Database
+mongoClient.connect(MONGODB_URI, function(err, db) {
+  if(!err) {
+    console.log("We are connected");
+    // Store the connection to the mongodb database on the aplication object
+    // under the name db so that I can access in another file
+    app.set('db', db);
+  }
+  else {
+    throw err;
+  }
+});
+
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(express.cookieParser('clonmellit'));
+app.use(express.session());
+
+app.use(express.bodyParser());
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -29,7 +57,9 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', routes.index);
-app.get('/users', user.list);
+app.post('/addPerson', routes.addPerson);
+app.get('/personsColours', routes.personsColours);
+app.post('/addColour', routes.addColour);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,83 @@
 
+var ObjectId = require('mongodb').ObjectID;
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+	var db = req.app.settings.db;
+
+	db.collection('cappPerson', function (err, personsCollection) {
+		if (err) throw err;
+
+		personsCollection.find().limit(10).toArray(function(err, resultsArray){
+			if (err) {throw err};
+
+			res.render('index', { personsArray: resultsArray });
+		})
+	});
+};
+
+exports.addPerson = function(req, res){
+	var db = req.app.settings.db;
+
+	var personsName = req.param('person');
+
+	if(!personsName) {
+		res.redirect('/');
+	} else {
+		db.collection('cappPerson', function (err, personsCollection) {
+			personsCollection.insert({name: personsName}, {w:1}, function (err, result) {
+				if (err) throw err;
+
+				res.redirect('/');
+			});
+		});
+	}
+
+};
+
+exports.personsColours = function(req, res) {
+	var db = req.app.settings.db;
+	var personsID = req.param('id');
+
+	if (!personsID) {
+		res.render('/');
+	} else {
+		var personsObjectID = new ObjectId(personsID);
+
+		req.session.personsOID = personsID;
+
+		db.collection('cappColours', function (err, coloursCollection) {
+			coloursCollection.find({person: personsObjectID}).limit(50).toArray(function (err, resultsArray){
+				if (err) throw err;
+
+				res.render('personsColours', {coloursArray: resultsArray});
+			});
+		});
+
+		
+	}
+	
+};
+
+exports.addColour = function(req, res) {
+	var db = req.app.settings.db;
+	var colour = req.param('colour');
+	var personsObjectID = new ObjectId(req.session.personsOID);
+
+	if (!colour) {
+		res.redirect('/personsColours?id='+req.session.personsOID);
+	} else {
+		db.collection('cappColours', function (err, coloursCollection) {
+			if (err) throw exception;
+
+			coloursCollection.insert({colour: colour, person: personsObjectID}, {w:1}, function(err, result) {
+				if (err) throw err;
+
+				res.redirect('/personsColours?id='+req.session.personsOID);
+			});
+		});
+	}
 };
